94c0f99719af9ef4ad0e2e0f32e5c4229cb493a3
Bernardo Casares Rosa <bcasares@stanford.edu>
2018-03-10 12:13:55 -0800

    small changes

--- update_plugins.py
+++ update_plugins.py
@@ -42,7 +42,6 @@ vim-multiple-cursors https://github.com/terryma/vim-multiple-cursors
 vim-fugitive https://github.com/tpope/vim-fugitive
 goyo.vim https://github.com/junegunn/goyo.vim
 vim-zenroom2 https://github.com/amix/vim-zenroom2
-syntastic https://github.com/scrooloose/syntastic
 vim-repeat https://github.com/tpope/vim-repeat
 vim-commentary https://github.com/tpope/vim-commentary
 vim-go https://github.com/fatih/vim-go
